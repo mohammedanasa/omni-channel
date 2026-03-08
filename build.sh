59a513d39dcddbd8ca37828898d82becbfc845a0
Mohammed Anas <mohammedanasa@outlook.com>
2026-03-08 13:43:30 +0530

    Add superuser and public tenant creation logic to build script

--- build.sh
+++ build.sh
@@ -19,3 +19,36 @@ python manage.py makemigrations $APPS
 
 python manage.py migrate_schemas --shared
 python manage.py migrate_schemas --tenant
+
+# Create superuser if it doesn't already exist
+python manage.py shell -c "
+from accounts.models import User
+if not User.objects.filter(email='$DJANGO_SUPERUSER_EMAIL').exists():
+    User.objects.create_superuser(
+        email='$DJANGO_SUPERUSER_EMAIL',
+        password='$DJANGO_SUPERUSER_PASSWORD',
+        first_name='Admin'
+    )
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
+
+# Create public tenant if it doesn't already exist
+python manage.py shell -c "
+from django_tenants.utils import get_tenant_model, get_tenant_domain_model
+from django.contrib.auth import get_user_model
+
+TenantModel = get_tenant_model()
+DomainModel = get_tenant_domain_model()
+User = get_user_model()
+
+if not TenantModel.objects.filter(schema_name='public').exists():
+    owner = User.objects.filter(is_superuser=True).first()
+    public_tenant = TenantModel(schema_name='public', name='Public', owner=owner)
+    public_tenant.save()
+    DomainModel(domain='$RENDER_EXTERNAL_HOSTNAME', tenant=public_tenant, is_primary=True).save()
+    print('Public tenant created')
+else:
+    print('Public tenant already exists')
+"
